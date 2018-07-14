89904fe653111b79ed0ce35ccf7c4beb7bc86b45
itaikatz <itai.j.katz@gmail.com>
2018-07-13 20:53:47 -0400

    contact form

---
+++ js/contact.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+
+ 	function validateEmail(email) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	}
+
+	$('#name')[0].addEventListener('input', function() {
+		$("#required-name").css({'visibility':'hidden'});
+	});
+	// $('#organization')[0].addEventListener('input', function() {
+	// 	$("#required-organization").css({'visibility':'hidden'});
+	// });
+	$('#email')[0].addEventListener('input', function() {
+		$("#required-email").css({'visibility':'hidden'});
+		validEmail = validateEmail($('#email')[0].value);
+		if (validEmail) {
+			$("#invalid-email").css({'visibility':'hidden'});	
+		}
+	});
+	$('#message')[0].addEventListener('input', function() {
+		$("#required-message").css({'visibility':'hidden'});
+	});
+
+$('#submit-button')[0].addEventListener('click', function(event) {
+	event.preventDefault();
+	valid = true
+	$('.required').css({'visibility':'hidden'});
+
+	if (!$('#name')[0].value) {	
+		$("#name").effect("shake");
+		$("#required-name").css({'visibility':'visible'});
+		valid = false
+	}
+
+	if (!$('#email')[0].value) {	
+		$("#email").effect("shake");
+		$("#required-email").css({'visibility':'visible'});
+		valid = false
+	}
+	else {
+		console.log('validating: ' + $('#email')[0].value)
+		validEmail = validateEmail($('#email')[0].value);
+		if (!validEmail) {
+			$("#email").effect("shake");
+			$("#invalid-email").css({'visibility':'visible'});
+		}
+		else {
+			$("#invalid-email").css({'visibility':'hidden'});	
+		}
+		valid = validEmail;
+	}
+
+	// /* validate email format here */
+
+	// if (!$('#organization')[0].value) {	
+	// 	$("#organization").effect("shake");
+	// 	$("#required-organization").css({'visibility':'visible'});
+	// 	valid = false
+	// }
+
+	if (!$('#message')[0].value) {	
+		$("#message").effect("shake");
+		$("#required-message").css({'visibility':'visible'});
+		valid = false
+	}
+
+  if (valid) {
+  	$('#form')[0].submit();
+  	// document.getElementById('form').submit();
+  }
+
+
+});
+
+    // $(".errBox").effect("shake");
+ // $form = $("#form"); 
+ //    $form.on("submit",function()
+ //    {
+ //       alert("submitting..");
+ //       //do ajax
+ //       $.ajax({
+ //                 url:<submit url>,
+ //                 type:post/get,
+ //                 success:function() { alert("done");  }
+ //             });
+ //    });
+ //     $form.submit();
+
+});
